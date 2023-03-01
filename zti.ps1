a78771e83d9ffa8a8f1776a73d36759cd27196e7
Henrik Skovgaard <hnrk@skvgrd.com>
2023-03-01 12:50:30 +0100

    yep

--- zti.ps1
+++ zti.ps1
@@ -1,4 +1,3 @@
-Write-Host  -ForegroundColor Cyan "Starting SeguraOSD's Custom OSDCloud ..."
 Start-Sleep -Seconds 5
 
 #Change Display Resolution for Virtual Machine
@@ -8,13 +7,8 @@ if ((Get-MyComputerModel) -match 'Virtual') {
 }
 
 #Make sure I have the latest OSD Content
-Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
 Install-Module OSD -Force
-
-Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
 Import-Module OSD -Force
 
 #Start OSDCloud ZTI the RIGHT way
-Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
-# Start-OSDCloud -OSVersion "Windows 11" -OSLanguage en-us -OSBuild 22H2 -OSEdition Pro -ZTI -Firmware -Restart
-Invoke-Expression -Command (Invoke-RestMethod -Uri sandbox.osdcloud.com)
+Start-OSDCloud -OSVersion "Windows 11" -OSLanguage en-us -OSBuild 22H2 -OSEdition Pro -ZTI -Firmware -Restart
