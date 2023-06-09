c8527fb0980d05477b24bc620740c08282e3ae4f
Henrik Skovgaard <hnrk@skvgrd.com>
2023-06-09 09:25:43 +0200

    Update Image from azure.ps1

--- ScriptPad/Image from azure.ps1
+++ ScriptPad/Image from azure.ps1
@@ -18,7 +18,7 @@ PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
 Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force
 Start /Wait PowerShell -NoL -C Install-Module OSD -Force
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/woodyard/dsgr/main/oobe_dk.ps1
+Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/woodyard/bls/main/oobe_dk.ps1
 '@
 $OOBECMD | Out-File -FilePath 'C:\Windows\System32\OOBE.cmd' -Encoding ascii -Force
 
